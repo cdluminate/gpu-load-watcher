2aab1d54a7dd23fb38e235d2e20e18aff1cabaec
Mo Zhou <cdluminate@gmail.com>
2023-10-04 17:16:23 -0400

    server: refresh interval 7

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ License: MIT/Expat
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="refresh" content="5" />
+    <meta http-equiv="refresh" content="7" />
     <title>Mo's GPU Watcher</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
