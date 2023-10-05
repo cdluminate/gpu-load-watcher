a6a36f0f463ad6cd7d43fcc511997b06e053ed63
Mo Zhou <cdluminate@gmail.com>
2023-10-05 13:48:55 -0400

    server: navbar fixed top

--- server.py
+++ server.py
@@ -41,7 +41,7 @@ License: MIT/Expat
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary" style="display: block" id="navbar">
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top" style="display: block" id="navbar">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">GPU Status</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
